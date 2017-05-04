641c086af211350a10a8223c9c0c9378a274cfb9
oisin tiernan <oisin tiernan>
2017-05-04 11:58:06 +0100

    called a repo, need to learn to format properly, once thats done at a repo to a node in neo4j, then do this recursively

---
+++ crawler/.stack-work/dist/ca59d0ab/build/autogen/Paths_crawler.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -fno-warn-implicit-prelude #-}
+module Paths_crawler (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "C:\\Users\\oisin\\Desktop\\CS7009\\CS7009\\crawler\\.stack-work\\install\\9320a514\\bin"
+libdir     = "C:\\Users\\oisin\\Desktop\\CS7009\\CS7009\\crawler\\.stack-work\\install\\9320a514\\lib\\x86_64-windows-ghc-8.0.2\\crawler-0.1.0.0-9t1m2uCPy39IMQF6l7wxJ2"
+dynlibdir  = "C:\\Users\\oisin\\Desktop\\CS7009\\CS7009\\crawler\\.stack-work\\install\\9320a514\\lib\\x86_64-windows-ghc-8.0.2"
+datadir    = "C:\\Users\\oisin\\Desktop\\CS7009\\CS7009\\crawler\\.stack-work\\install\\9320a514\\share\\x86_64-windows-ghc-8.0.2\\crawler-0.1.0.0"
+libexecdir = "C:\\Users\\oisin\\Desktop\\CS7009\\CS7009\\crawler\\.stack-work\\install\\9320a514\\libexec"
+sysconfdir = "C:\\Users\\oisin\\Desktop\\CS7009\\CS7009\\crawler\\.stack-work\\install\\9320a514\\etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "crawler_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "crawler_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "crawler_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "crawler_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "crawler_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "crawler_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "\\" ++ name)
--- crawler/src/Lib.hs
+++ crawler/src/Lib.hs
@@ -1,26 +1,43 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE DeriveAnyClass       #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE StandaloneDeriving   #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE RecordWildCards #-}
+
+
 module Lib
     ( startApp
     , app
     ) where
 
+import Control.Monad.IO.Class
 import Data.Aeson
 import Data.Aeson.TH
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
+import Servant.API
+import Servant.Client
+import GHC.Generics
+import Data.Text
+import qualified Data.ByteString.Char8 as DBC
+import qualified GitHub.Endpoints.Repos as Github
+import qualified GitHub.Data.Repos as GithubData
 
-data User = User
-  { userId        :: Int
-  , userFirstName :: String
-  , userLastName  :: String
-  } deriving (Eq, Show)
 
-$(deriveJSON defaultOptions ''User)
+data Response_crawl = Response_crawl { result :: String
+                                     } deriving (Show,Eq,Generic,ToJSON,FromJSON)
 
-type API = "users" :> Get '[JSON] [User]
+
+
+type API = "initialise_crawl" :> Get '[JSON] Response_crawl
+
 
 startApp :: IO ()
 startApp = run 8080 app
@@ -32,9 +49,33 @@ api :: Proxy API
 api = Proxy
 
 server :: Server API
-server = return users
+server = initialise_crawl
+  where
+  	initialise_crawl :: Handler Response_crawl
+  	initialise_crawl = do
+  		liftIO $ get_repo "mike"
+  		return $ Response_crawl "hello"
+  		
 
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
+
+
+get_repo :: Text -> IO()
+get_repo name = liftIO $ do
+	user_repository <- Github.userRepos "mike-burns" GithubData.RepoPublicityAll
+  	case user_repository of
+  		(Left error) -> do 
+  			putStrLn $ "Error: " ++ (show error)
+  		(Right repos) -> do
+  			--let desc = (GithubData.repoDescription repos)
+  			let repo = repos :: Github.Repo
+  			putStrLn $ "working"
+
+formatRepo :: Github.Repo -> IO()
+formatRepo repo = do
+	let desc = GithubData.repoDescription repo
+	putStrLn (show desc)
+
+
+--Need to make an initial function which just gets one repository and then adds it to the neo4j graph.
+
+
